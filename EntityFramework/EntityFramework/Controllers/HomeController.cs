46d84828873d07202f67bcb3bae23da84432b58f
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2022-03-10 03:50:46 +0400

    EntityFrameworkMigration

---
+++ EntityFramework/EntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using EntityFramework.Data;
+using EntityFramework.Models;
+using EntityFramework.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+            SliderDetail details = await _context.SliderDetails.FirstOrDefaultAsync();
+
+            HomeVM homeVM = new HomeVM
+            {
+                Sliders = sliders,
+                Detail = details,
+            };
+            return View(homeVM);
+        }
+    }
+}
